43ae8844205b6183b74edf4aaf4e9b98adae1228
hernot <christoph@out-world.com>
2019-10-26 22:15:53 +0200

    Pylint local transform plugin proxy
    On dynmaic classes, and modules pylint has hard time to infer whether
    dynamically added attributes are valid or not. Through the --load-plugin
    command line parameter and the load-plugin configuraiton file parameter
    additional modules providing the appropriate knowledge can be loaded and
    activated.
    
    The disadvantage is that on complex projects all plugins have to be
    specified either every time on commandline or permanently in
    configuration file. Which is not allways desired or tedious.
    
    The plugin proxy module allows to store additional plugin modules along
    with the module it should provide transformatoin and type infrerences
    for. In case the module defining a monitored astroid node type is
    encountered for the first time the corresponding <modulename>.ast.py
    plugin module is loaded if present on the path the <module>.py file was
    loaded from.

---
+++ pylint_plugin_proxy.py
@@ -0,0 +1,87 @@
+"""
+astroid plugin for loading transforms and in future possilby
+type inferences from module level astoid plugin modules instead
+of specifying on command line or in pyling config
+"""
+
+# TODO if reqired add typeinference hint proxy
+
+import os.path as ospath
+import importlib.util
+import astroid
+
+def register(linter):
+    pass
+
+_knownmodules = dict()
+_nomodule = ()
+_nodesofinterest = (
+    astroid.ClassDef,
+)
+
+def transform_proxy(node):
+    """
+    loads custom astroid transform definitions from local
+    astroid plugin files and makes them available to any
+    parser or linter utilizing them. for example pylint
+    """
+    if not isinstance(node, _nodesofinterest):
+        # node not of interest
+        return
+    _nodename = node.qname()
+    _nodemodule = _knownmodules.get(_nodename)
+    if _nodemodule:
+        # transform plugin already loaded do not bother any further
+        return
+    # find defining module
+    _module = node
+    while _module:
+        if not isinstance(_module, astroid.Module):
+            _module = _module.parent
+            continue
+        break
+    if _module is None:
+        # no module found along ast tree ignore node
+        _knownmodules[_nodename] = _nomodule
+        return
+    # check if module defining node has already been checked
+    _modulename = _module.qname()
+    _nodemodule = _knownmodules.get(_modulename)
+    if _nodemodule:
+        # node is defined by module corresponding astoid transforms and type inferences
+        # have already been loaded
+        _knownmodules[_nodename] = _nodemodule
+        return
+    # check if on the same path a astroid transform and type inference plugin
+    # can be found and import it
+    if not _module.file:
+        _knownmodules[_modulename] = _knownmodules[_nodename] = _nomodule
+        return
+    _package = _module
+    _moduleastpath,_fn = ospath.split(_module.file)
+    _moduleprefix = ospath.splitext(_fn)[0]
+    _moduleastname = "{}_astroid".format(_moduleprefix)
+    _moduleastfile = ospath.join(_moduleastpath, "{}.ast.py".format(_moduleprefix))
+    _modulespec = importlib.util.spec_from_file_location(_moduleastname,_moduleastfile)
+    if _modulespec is None:
+        # remember that no ast module exists for node and corresponding module
+        _knownmodules[_modulename] = _knownmodules[_nodename] = _nomodule
+        return
+    # try to import plugin module
+    _nodemodule = importlib.util.module_from_spec(_modulespec)
+    if _nodemodule is None:
+        _knownmodules[_modulename] = _knownmodules[_nodename] = _nomodule
+        return
+    try:
+        _modulespec.loader.exec_module(_nodemodule)
+    except FileNotFoundError:
+        # remember that no ast module exists for node and corresponding module
+        _knownmodules[_modulename] = _knownmodules[_nodename] = _nomodule
+        return
+
+    _knownmodules[_modulename] = _knownmodules[_nodename] = _modulespec
+    # done let the local ast node transforms for astroid.ClassDef nodes do
+    # the transforms and typeinferences just imported
+
+for _interestedin in _nodesofinterest:
+    astroid.MANAGER.register_transform(_interestedin, transform_proxy)
